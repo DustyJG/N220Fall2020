1f17ec3fe7751e9e8f1152e48fcc34bf1e65f63f
DustyJG <69523014+DustyJG@users.noreply.github.com>
2020-08-28 11:16:09 -0400

    Update app.js

--- Labs/Lab1/js/app.js
+++ Labs/Lab1/js/app.js
@@ -1,20 +1,16 @@
 function setup() {
     
-    var red = "#d62943";
-
-    var blue = "#0bcee8";
-
-    var mustard = "#edda87";
+    var red = "#FF0000";
 
     var startX = 50;
     
     createCanvas(800,600);
     noStroke();
-    background(mustard);
+    background(237, 218, 135);
     
     stroke(217, 135, 237);
     strokeWeight(10);
-    fill(blue);
+    fill(11, 206, 232);
     rect(startX, 300, 300, 50);
     
     stroke(217, 135, 237);
