5e93a2a91b2846bbeb9e44e3444405fa9674012d
DustyJG <69523014+DustyJG@users.noreply.github.com>
2020-08-27 21:14:12 -0400

    Lab1 FInal Commit

--- Labs/Lab1/js/app.js
+++ Labs/Lab1/js/app.js
@@ -1,3 +1,23 @@
-function setup(){
+function setup() {
+    
+    var blue="214, 41, 67";
+
+    var red="111, 208, 232";
+    
     createCanvas(800,600);
+
+    background(237, 218, 135);
+
+    stroke(217, 135, 237);
+    strokeWeight(10);
+    fill(blue);
+    rect(300, 300, 300, 50);
+
+    stroke(217, 135, 237);
+    strokeWeight(10);
+    fill(red);
+    circle(100, 100, 30);
+
+    stroke(5);
+    line(0, 500, 300, 20);
 }
