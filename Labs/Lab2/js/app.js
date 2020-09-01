978674ff1a0bb046bd638d4878e72101e70ba596
DustyJG <69523014+DustyJG@users.noreply.github.com>
2020-08-31 22:07:11 -0400

    Update app.js

--- Labs/Lab2/js/app.js
+++ Labs/Lab2/js/app.js
@@ -1,12 +1,17 @@
 let xPos = 0;
-let yPos = 0:
+let yPos = 0;
+
+let xSpeed = 1;
+let ySpeed = 1;
 
 function setup() {
-    createCanvas(400, 300);
+    createCanvas(800, 600);
 }
 
 function draw() {
-    xPos = xPos + 2;
-    yPos = yPos + 1;
+    background(62, 237, 108);
+    xPos = xPos + xSpeed;
+    yPos = yPos + ySpeed;
+    fill(19, 71, 26);
     circle(xPos, yPos, 30);
 }
