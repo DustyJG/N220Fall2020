c4c3d209385f19d099e1e54122b24e56cbc8ecd3
DustyJG <69523014+DustyJG@users.noreply.github.com>
2020-08-31 20:29:27 -0400

    Update app.js

--- Labs/Lab2/js/app.js
+++ Labs/Lab2/js/app.js
@@ -1,5 +1,25 @@
-let spices = 0;
-function draw() {
-    spices = spices - 1;
-    console.log (spices);   
+function setup() {
+    
+    var red = "#FF0000";
+
+    var startX = 50;
+    
+    createCanvas(800,600);
+    noStroke();
+    background(237, 220, 135);
+    
+    stroke(217, 135, 237);
+    strokeWeight(5);
+    fill(11, 206, 232);
+    rect(startX, 300, 300, 50);
+    
+    stroke(217, 135, 237);
+    strokeWeight(5);
+    fill(red);
+    circle(startX, 100, 30);
+    
+    
+    stroke(10);
+    line(100, 175, 300, 20);
+    
 }
