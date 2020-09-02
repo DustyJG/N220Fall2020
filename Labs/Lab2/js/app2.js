568d772a366d8ee904f05b3463246f867b66ba70
DustyJG <69523014+DustyJG@users.noreply.github.com>
2020-09-02 15:28:02 -0400

    Update app2.js

--- Labs/Lab2/js/app2.js
+++ Labs/Lab2/js/app2.js
@@ -1,13 +1,19 @@
 let xPos = 0;
 let yPos = 0;
 
+let xSpeed = 2;
+let ySpeed = 0;
+
 function setup() {
     createCanvas(400, 300);
 }
 
 function draw() {
     background(green)
-    xPos++;
+
+    xPos = xPos + xSpeed;
+    yPos = yPos + ySpeed;
+    
     circle(xPos, 200, 30)
     fill(blue);
     if( xPos > 164 ) {
