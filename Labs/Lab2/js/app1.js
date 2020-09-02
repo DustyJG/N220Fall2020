78c08af4f81a5669574567594e834ebb68c744c9
DustyJG <69523014+DustyJG@users.noreply.github.com>
2020-09-02 14:20:18 -0400

    Update app1.js

--- Labs/Lab2/js/app1.js
+++ Labs/Lab2/js/app1.js
@@ -1,25 +1,5 @@
-function setup() {
-    
-    var red = "#FF0000";
-
-    var startX = 50;
-    
-    createCanvas(800,600);
-    noStroke();
-    background(237, 220, 135);
-    
-    stroke(217, 135, 237);
-    strokeWeight(5);
-    fill(11, 206, 232);
-    rect(startX, 300, 300, 50);
-    
-    stroke(217, 135, 237);
-    strokeWeight(5);
-    fill(red);
-    circle(startX, 100, 30);
-    
-    
-    stroke(10);
-    line(100, 175, 300, 20);
-    
-}
+let counter = 0;
+function draw() {
+        counter = counter + 1;
+        console.log(counter);
+    }
