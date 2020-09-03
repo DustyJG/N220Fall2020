785b1dfb879f129ed4d3643066d7e501459f22db
DustyJG <69523014+DustyJG@users.noreply.github.com>
2020-09-03 14:31:43 -0400

    Update app5.js

---
+++ Labs/Lab2/js/app5.js
@@ -0,0 +1,16 @@
+let xPos = 400;
+let yPos = 300;
+let xSpeed = 1;
+var startX = 0;
+
+function setup() {
+    createCanvas(800, 600);
+}
+
+function draw() {
+    background(255)
+
+startX = startX +1;
+
+circle(xPos, yPos, startX);
+}
