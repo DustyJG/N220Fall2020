36a75d6405d885536e584c30e6697fbb2dde1495
DustyJG <69523014+DustyJG@users.noreply.github.com>
2020-09-03 11:39:50 -0400

    Update app3.js

--- Labs/Lab2/js/app3.js
+++ Labs/Lab2/js/app3.js
@@ -2,22 +2,8 @@ let xPos = 0;
 let yPos = 0;
 
 function setup() {
-    createCanvas(400, 300);
+    createCanvas(800, 600);
 }
 function draw() {
     background(30, 200, 30)
-}
-
-let value = 0;
-function draw() {
-  fill(value);
-}
-
-circle(xPos, yPos, 30,);
-
-function mouseMoved() {
-  value = value + 1;
-  if (value > 255) {
-    value = 0;
-  }
 }
