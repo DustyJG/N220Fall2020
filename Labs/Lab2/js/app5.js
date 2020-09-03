50a3e32152b25d82e186e1aedf280877974b42c1
DustyJG <69523014+DustyJG@users.noreply.github.com>
2020-09-03 16:37:53 -0400

    Final Commit

--- Labs/Lab2/js/app5.js
+++ Labs/Lab2/js/app5.js
@@ -1,7 +1,7 @@
 let xPos = 400;
 let yPos = 300;
 let xSpeed = 1;
-let startX = 0;
+let startX = 1;
 function setup() {
     createCanvas(800, 600);
 }
@@ -9,7 +9,7 @@ function draw() {
     background(255)
 startX = startX + 1;
 if( startX = 200 ) {
-    startX = 0;
+    startX = 1;
 }
 circle(xPos, yPos, startX);
 }
