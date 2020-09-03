3dd4d0007d0c477204d5f93b770a45748201adf4
DustyJG <69523014+DustyJG@users.noreply.github.com>
2020-09-03 11:55:26 -0400

    Update app3.js

--- Labs/Lab2/js/app3.js
+++ Labs/Lab2/js/app3.js
@@ -1,9 +1,14 @@
 let xPos = 0;
-let yPos = 0;
+let yPos = 300;
 
 function setup() {
     createCanvas(800, 600);
 }
 function draw() {
     background(30, 200, 30);
+}
+
+function draw() {
+  xPos++;
+  circle(xPos, 400, 30);
 }
