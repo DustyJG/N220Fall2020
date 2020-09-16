0812c219adbd763a96eb43885f1d8d45f70f7a81
DustyJG <69523014+DustyJG@users.noreply.github.com>
2020-09-16 16:21:26 -0400

    Update app99.js

--- Labs/Lab3/js/app99.js
+++ Labs/Lab3/js/app99.js
@@ -7,15 +7,21 @@ function setup() {
     createCanvas(400, 300);
     background(100);
     //For loop syntax
-    for(var i = 15; i > 0; i--);
+    for( var i = 15; i > 0; i--); {
 
-    var color = Math.random();
-    console.log(color);
-
-    stroke(255 - (i*15));
-
-    circle(200, 150, i * 10);
+        var randColor = Math.random() * 255;
+        fill(randColor);
+    
+        stroke(255 - (i*15));
+    
+        circle(200, 150, i * 10);
+    }
 }
+
+function draw() {
+
+    //noFill();
 }
+
 
 
